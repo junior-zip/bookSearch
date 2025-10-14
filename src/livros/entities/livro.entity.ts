e8635f097b1e390ad6b381e8154c114fd47ead62
Junior Teixeira <juniorteixeira@Mac-mini-de-Junior.local>
2025-10-14 15:28:14 -0300

    feat: Create DTOs and livro.entity.ts

--- src/livros/entities/livro.entity.ts
+++ src/livros/entities/livro.entity.ts
@@ -1,42 +1,46 @@
-import { IsDate } from 'class-validator';
-import { Column, CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
+@Entity()
 export class Livro {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ length: 13 })
+  ISBN: string;
+
   @Column({ length: 150 })
   titulo: string;
 
-  @Column({ length: 255 })
+  @Column({ type: 'text' })
   descricao: string;
 
   @Column({ length: 100 })
   genero: string;
 
-  @Column({ length: 13 })
-  ISBN: string;
-
-  @Column()
+  @Column({ nullable: true })
   imagem: string;
 
-  @Column()
+  @Column({ nullable: true })
   link: string;
 
-  @Column({ length: 3 })
-  IdadeMin: number;
-
-  @Column({ length: 5 })
-  paginas: number;
+  @Column({ nullable: true })
+  idadeMin: number;
 
   @Column()
-  @IsDate()
+  paginas: number;
+
   @Column({ type: 'date', nullable: true })
   dataPublicacao: Date;
 
   @CreateDateColumn()
   createdAt: Date;
 
-  @CreateDateColumn()
+  @UpdateDateColumn()
   updatedAt: Date;
 }
