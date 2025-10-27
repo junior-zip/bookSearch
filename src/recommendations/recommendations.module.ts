d421d78548f7bbfa39177d65ea65f31b7f467143
Junior Teixeira <juniorteixeira@Mac-mini-de-Junior.local>
2025-10-27 11:48:48 -0300

    Update modules and imports

--- src/recommendations/recommendations.module.ts
+++ src/recommendations/recommendations.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { RecommendationsService } from './recommendations.service';
 import { RecommendationsController } from './recommendations.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Recommendation } from './entities/recommendation.entity';
+import { LivrosModule } from 'src/livros/livros.module';
+import { UsuarioModule } from 'src/usuario/usuario.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Recommendation]),
+    LivrosModule,
+    UsuarioModule,
+  ],
   controllers: [RecommendationsController],
   providers: [RecommendationsService],
+  exports: [RecommendationsService],
 })
 export class RecommendationsModule {}
